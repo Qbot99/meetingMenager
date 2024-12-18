3530a00944d588eda50b1afb892b2f79f2ad1fe0
Hubert <hkubot@icloud.com>
2024-12-18 17:24:59 +0100

    You can now edit the meeting

---
+++ ScrumDinger/View/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  ScrumDinger
+//
+//  Created by Hubert Kubot on 13/12/2024.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
